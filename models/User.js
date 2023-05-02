2392b2814e9b63489a70e7d0db753fceb007138f
AaronDreyer <aaronottaway@gmail.com>
2023-05-02 13:36:20 -0400

    Models
    Ive added the models for the User and Thoughts based on the structure provided in the course material. However the getter portion of the models im confused about. I need to find a way to include a date function so I can establuish the current timestap for the createdAt portion of the model format in the THoughts Model.

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/,
+        },
+        thoughts: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+            }
+        ],
+        friends: [ 
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.firends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
+
+// User
+
+// username
+// String
+// Unique
+// Required
+// Trimmed
+
+// email
+// String
+// Required
+// Unique
+// Must match a valid email address (look into Mongoose's matching validation)
+
+// thoughts
+// Array of _id values referencing the Thought model
+
+// friends
+// Array of _id values referencing the User model (self-reference)
