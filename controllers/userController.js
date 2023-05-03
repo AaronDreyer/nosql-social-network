ce0764ce79ea8db82b9ead2fdc336535fc64d6c4
AaronDreyer <aaronottaway@gmail.com>
2023-05-03 13:55:06 -0400

    getSingleUser
    I added the controller for retrieving a single user based on id using the mongodb findOne method, the populate method to populate thoughts and friends of the user document with their corresponding documents from the thoughts and users collections (path specifiing the name of the field to populate and select to exclude the version), and the then(dbUserData to retrieve the user document.

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,5 +10,30 @@ const userController = {
             console.log(err);
             res.sendStatus(400);
         });
-    }
+    },
+
+getSingleUser({ params }, res) {
+    User.findOne({ _id: params.id })
+    .populate({
+        path: 'thoughts',
+        select: '-__v'
+    })
+    .populate({
+        path: 'friends',
+        select: '-__v'
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.stauts(404).json({ message: 'No User found with this ID!' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+},
+
+
 }
