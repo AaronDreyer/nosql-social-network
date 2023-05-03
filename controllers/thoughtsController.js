16df2cf6175779c83d2a53c8e6426fae3e9389ed
AaronDreyer <aaronottaway@gmail.com>
2023-05-03 14:59:06 -0400

    getSingleThought
    Very similar to the getSingleUser hwoever I need to populate the reactions asccoiated with the Thought id in order to show both. I did not need to populate friends as that is associated with users and not individual thoughts.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -11,4 +11,31 @@ const thoughtsController = {
             res.sendStatus(400);
         });
     },
+
+    getSingleThought({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No Thoughts found with this ID!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    createThought({ body }, res) {
+        Thought.create(body)
+        
+    }
+
 }
