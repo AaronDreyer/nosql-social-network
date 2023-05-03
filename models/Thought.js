062fbecc7954b3fbe03f3340ae24662be084b5d3
AaronDreyer <aaronottaway@gmail.com>
2023-05-03 11:56:12 -0400

    Moment.js
    In order to use the getter method to show a timestamp I used moment.js as we did in a previous chalenge. I saw in some other example that you can create a helper.js file to format the date but the mongodb documentation showed a much easier way of dsiplay time using moment.

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
 const reactionSchema = new Schema(
     {
@@ -18,7 +19,7 @@ const reactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get:
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         }
     },
         {
@@ -41,7 +42,7 @@ const thoughtsSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get:
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         },
         username: {
             type: String,
